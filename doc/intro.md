cdd9a9996d6642db25a740767ae903b7054c73b3
standinga <standinga@o2.pl>
2016-03-12 21:47:33 +0100

    empty

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to scsrapper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/scsrapper/core_test.clj
@@ -0,0 +1,7 @@
+(ns scsrapper.core-test
+  (:require [clojure.test :refer :all]
+            [scsrapper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
